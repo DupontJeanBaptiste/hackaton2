d844e7fb97ba645b27b445007df8e7dce18046ef
DupontJeanBaptiste <jbdupont67@gmail.com>
2022-06-29 17:33:22 +0200

    db creation make the migration and the migrate

---
+++ src/Entity/User.php
@@ -0,0 +1,248 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
+    private $email;
+
+    #[ORM\Column(type: 'json')]
+    private $roles = [];
+
+    #[ORM\Column(type: 'string')]
+    private $password;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $firstname;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $lastname;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $team;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $post;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $creationdate;
+
+    #[ORM\OneToOne(targetEntity: Parameter::class, cascade: ['persist', 'remove'])]
+    private $parameter;
+
+    #[ORM\ManyToMany(targetEntity: Project::class, inversedBy: 'users')]
+    private $project;
+
+    #[ORM\ManyToMany(targetEntity: Tache::class, inversedBy: 'users')]
+    private $tache;
+
+    public function __construct()
+    {
+        $this->project = new ArrayCollection();
+        $this->tache = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getTeam(): ?string
+    {
+        return $this->team;
+    }
+
+    public function setTeam(string $team): self
+    {
+        $this->team = $team;
+
+        return $this;
+    }
+
+    public function getPost(): ?string
+    {
+        return $this->post;
+    }
+
+    public function setPost(string $post): self
+    {
+        $this->post = $post;
+
+        return $this;
+    }
+
+    public function getCreationdate(): ?string
+    {
+        return $this->creationdate;
+    }
+
+    public function setCreationdate(string $creationdate): self
+    {
+        $this->creationdate = $creationdate;
+
+        return $this;
+    }
+
+    public function getParameter(): ?Parameter
+    {
+        return $this->parameter;
+    }
+
+    public function setParameter(?Parameter $parameter): self
+    {
+        $this->parameter = $parameter;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Project>
+     */
+    public function getProject(): Collection
+    {
+        return $this->project;
+    }
+
+    public function addProject(Project $project): self
+    {
+        if (!$this->project->contains($project)) {
+            $this->project[] = $project;
+        }
+
+        return $this;
+    }
+
+    public function removeProject(Project $project): self
+    {
+        $this->project->removeElement($project);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Tache>
+     */
+    public function getTache(): Collection
+    {
+        return $this->tache;
+    }
+
+    public function addTache(Tache $tache): self
+    {
+        if (!$this->tache->contains($tache)) {
+            $this->tache[] = $tache;
+        }
+
+        return $this;
+    }
+
+    public function removeTache(Tache $tache): self
+    {
+        $this->tache->removeElement($tache);
+
+        return $this;
+    }
+}
