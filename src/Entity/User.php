c71fc8d2ce13b9b83dc16d6a8532e581dddc2136
yecine18 <yecine.kefi1@gmail.com>
2022-06-30 11:05:29 +0200

    login okay

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,10 +6,12 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -37,9 +39,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(type: 'string', length: 255)]
     private $post;
-
-    #[ORM\Column(type: 'string', length: 255)]
-    private $creationdate;
 
     #[ORM\OneToOne(targetEntity: Parameter::class, cascade: ['persist', 'remove'])]
     private $parameter;
@@ -174,18 +173,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setPost(string $post): self
     {
         $this->post = $post;
-
-        return $this;
-    }
-
-    public function getCreationdate(): ?string
-    {
-        return $this->creationdate;
-    }
-
-    public function setCreationdate(string $creationdate): self
-    {
-        $this->creationdate = $creationdate;
 
         return $this;
     }
