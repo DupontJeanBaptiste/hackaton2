ed5ed7a390d0787f865ba15a8fdf2091af880fa6
Hugo Marx <marx.hugo@gmail.com>
2022-06-30 22:49:02 +0200

    AJAX project Add/remove

--- assets/app.js
+++ assets/app.js
@@ -10,14 +10,42 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+import { add } from '@hotwired/stimulus';
 
 const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
 
-
 //Sauvegarde projet par User
 
-document
+let addButtons = document.getElementsByClassName('project-add');
 
+
+for (let i = 0; i < addButtons.length; i++) {
+	addButtons[i].addEventListener('click', function addProject(e){
+        e.preventDefault();
+        let requestPath = e.currentTarget.href;
+        console.log(requestPath);
+    
+        fetch(requestPath)
+            .then((res) => res.json())
+            .then(function (res) {
+                let addButton = e.srcElement;
+                  console.log(addButton);
+                if (res.isOnProject) {
+                    addButton.classList.remove('btn-primary');
+                    addButton.classList.add('btn-success');
+                    addButton.innerHTML = 'Projet enregistré !';
+                } else {
+                    addButton.classList.remove('btn-success');
+                    addButton.classList.add('btn-primary');
+                    addButton.innerHTML = 'Rejoindre le projet';
+                }
+            });
+    });
+}
+
+function addProject(e) {
+ 
+}
