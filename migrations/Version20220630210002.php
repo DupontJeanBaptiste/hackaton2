7d9a31c9e85a9b640822c17c5f203a30634da17b
Hugo Marx <marx.hugo@gmail.com>
2022-06-30 23:28:03 +0200

    Branch error fix

--- migrations/Version20220630210002.php
+++ migrations/Version20220630210002.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220630115711 extends AbstractMigration
+final class Version20220630210002 extends AbstractMigration
 {
     public function getDescription(): string
     {
